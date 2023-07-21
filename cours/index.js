30ea3e1bf3b693330631f97cd2596155d7f40562
Aboubacar <minonaboubacars@gmail.com>
2023-07-21 14:42:39 +0000

    fin_de_travail

--- cours/index.js
+++ cours/index.js
@@ -169,10 +169,10 @@ document.body.addEventListener(
 
 //----------------------------------------------------------
 // Stop propagation
-// questionContainair.addEventListener("click", (e) => {
-//   alert("Test !");
-//   e.stopPropagation();
-// });
+questionContainair.addEventListener("click", (e) => {
+  alert("Test !");
+  e.stopPropagation();
+});
 
 // removeEventListener
 
@@ -193,10 +193,83 @@ btn2.addEventListener("click", () => {
   confirm("Voullez vous vraiment vous trompez ?");
 }); 
  
-
-
+//  prompt
+btn1.addEventListener("click", () => {
+  let answer =prompt("Entrez votre nom !");
   
+  questionContainair.innerHTML += "<h3>Bravo " + answer + "</h3>";
+});
+
+// Timer, compte à rebours
+setTimeout(() => {
+  questionContainair.style.borderRadius = "300px";
+}, 2000);
+
+// let interval = setInterval(() => {
+//   document.body.innerHTML += `
+//   <div class='box'>
+//   <h2>Nouvelle Boite !<h2>
+//   </div>
+//   `;
+// }, 1000);
+
+// document.body.addEventListener("click", (e) => {
+//   e.target.remove();   
+//   clearInterval(interval);
+// });
 
 
 
+// Location
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// console.log(location.search);
+// location.replace("http://lequipe.fr");
 
+// window.onload = () => { 
+//  location.href = "http:twitter.fr";
+// };
+
+// Navigator
+// console.log(navigator.userAgent);
+
+// https:developer.mozilla.org/fr/docs/web/API/Geolocation/getCurrentPosition
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
+
+// function success(pos) {
+//   var crd = pos.coords;
+  
+//   console.log("Votre position actuelle est :");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude : ${crd.longitude}`);
+//   console.log(`La précision est de ${crd.accuracy}
+//   mèttre.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERREUR (${err.code}): ${err.message}`)
+//   ;
+// }
+
+// navigator.geolocation.getCurrentPosition(success,
+// error, options);
+
+
+// History 
+// console.log(history)
+// window.history.back();
+// history.go(-2)
+
+//--------------------------------------------------------------- 
+// setproperty
+window.addEventListener("mousemove", (e) => {
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
+});
+ 
